d77c2ee406a67bcb718f7adc141c1c525c5506a0
sleepingkingstudios <merlin@sleepingkingstudios.com>
2013-01-11 16:09:33 -0500

    Ported AbstractController and specs, added docs

---
+++ lib/controllers/abstract_controller.rb
@@ -0,0 +1,136 @@
+# lib/controllers/abstract_controller.rb
+
+require 'controllers/controllers'
+require 'controllers/mixins/actions_base'
+
+module Mithril::Controllers
+  # Base class for Mithril controllers. Extending controller functionality
+  # can be implemented either through direct class inheritance, e.g.
+  # 
+  #   ModuleController > ProxyController > AbstractController
+  # 
+  # or through mixing in shared functionality with a Mixin, but all controllers
+  # ought to extend AbstractController unless you have a very compelling reason
+  # otherwise.
+  class AbstractController
+    extend Mithril::Controllers::Mixins::ActionMixin
+    
+    mixin Mithril::Controllers::Mixins::ActionsBase
+    
+    # Returns a singleton-ish instance to avoid unnecessary setup/teardown cost
+    # during responses. Should be safe because controllers manage state through
+    # the session parameter passed in to invoke_action and invoke_command, and
+    # any internal state should be cleared at the end of each request.
+    def self.instance
+      @instance ||= self.new
+    end # class method instance
+    
+    def initialise
+      # Do something. Or don't, that's okay too.
+    end # constructor
+    
+    def class_name
+      self.class.name.split("::").last
+    end # accessor class_name
+    private :class_name
+    
+    #########################
+    ### Executing Actions ###
+    
+    # Evaluates text input in the context of the passed-in session. The text is
+    # processed into a list of words, which are then matched against the
+    # controller's actions to determine which action (if any) to invoke. The
+    # matching is greedy, so "go to town" will match a :go_to action before it
+    # will match "go".
+    # 
+    # If a match is found, the corresponding action is invoked with the session
+    # and the remaining words as arguments. For example, if "go to town"
+    # matched the :go_to action, then invoke_action(session, :go_to, ["town"])
+    # would be called.
+    # 
+    # If a match is not found, returns a default message string.
+    # 
+    # === Parameters
+    # * session: Expects a hash (can be empty). Probably breaks if you pass in
+    #   nil, or something that isn't a hash.
+    # * text: Expects a string composed of one or more words, separated by
+    #   whitespace or hyphens.
+    # 
+    # === Returns
+    # The result of invoke_action, if an action was found (typically a string).
+    # Otherwise returns a default message string.
+    #--
+    # === Steps for processing text input:
+    # 
+    # 1.  User or client sends text input.
+    # 2.  Pre-process text input (strip, etc).
+    # 3.  Split input into words.
+    # 4.  Identify command and arguments.
+    #     1.  Let len = words.count.
+    #     2.  If 0 == len, goto 5.
+    #     3.  For i = len, i >= 0, --i
+    #         1.  Let command = words[0..i], arguments = words[i..len]
+    #         2.  If command.snakify is an action, goto 6.
+    # 5.  No valid command, return text output.
+    # 6.  Let action = actions(command.snakify).
+    # 7.  Output action[session, arguments]
+    #++
+    def invoke_command(session, text)
+      # Mithril.logger.debug "#{class_name}.invoke_command(), text = #{text.inspect}"
+      
+      command, args = self.parse_command text
+      
+      output = nil
+      output = self.invoke_action(session, command, args) unless command.nil?
+      
+      output || "I'm sorry, I don't know how to \"#{text}\". Please try another" +
+        " command, or enter \"help\" for assistance."
+    end # method invoke_command
+    
+    # Takes a string input and separates into words, then identifies a matching
+    # action (if any) and remaining arguments. Returns both the command and the
+    # arguments array, so usage can be as follows:
+    #   command, args = parse_command(text)
+    # 
+    # === Parameters
+    # * text: Expects a string composed of one or more words, separated by
+    #   whitespace or hyphens.
+    # 
+    # === Returns
+    # A two-element array consisting of the command and an array of the
+    # remaining text arguments (if any), or [nil, nil] if no matching action
+    # was found.
+    def parse_command(text)
+      text = self.preprocess_input(text)
+      
+      words = self.wordify text
+      
+      key  = nil
+      args = []
+      
+      while 0 < words.count
+        key = words.join('_').intern
+        return key, args if self.has_action? key
+        
+        args.unshift words.pop
+      end # while
+      
+      return nil, nil
+    end # method parse_command
+    
+    def preprocess_input(text) # :nodoc:
+      # Mithril.logger.debug "#{class_name}.preprocess_input(), text = #{text.inspect}"
+      
+      text.strip.downcase.
+        gsub(/(\-+|\s+)/, ' ').
+        gsub(/[\"?!-',.:\(\)\[\]\;]/, '')
+    end # method preprocess_input
+    
+    # Wordify. Words fail me, or perhaps I have failed them.
+    def wordify(text) # :nodoc:
+      # Mithril.logger.debug("#{class_name}.wordify(), text = #{text}")
+      
+      text.split(/\s+/)
+    end # method wordify
+  end # class AbstractController
+end # module
--- lib/controllers/mixins/actions_base.rb
+++ lib/controllers/mixins/actions_base.rb
@@ -3,26 +3,42 @@
 require 'controllers/mixins/mixins'
 
 module Mithril::Controllers::Mixins
+  # Core functions for implementing a command+args response model. ActionsBase
+  # should be mixed in to controllers, either directly or via an intermediate
+  # Mixin that implements default or shared actions.
   module ActionsBase
     extend ActionMixin
     
+    # These methods get extend-ed into the class of the controller through the
+    # magic of Mixin.
     module ClassMethods
+      # Defines an action to which the controller will respond. Currently,
+      # only the action key and block are implemented, but for forward
+      # compatibility the method also accepts an optional parameter hash.
+      # 
+      # Best practice is to use snake_case, e.g. all lower-case letters, with
+      # words separated by underscores. It *ought* to work anyway, but caveat
+      # lector.
       def define_action(key, params = {}, &block)
         key = key.to_s.downcase.gsub(/\s+|\-+/,'_').intern
         
-        params[:method_name] ||= :"action_#{key}"
-        
-        define_method params[:method_name], &block
+        define_method :"action_#{key}", &block
         
         @actions ||= {}
         @actions[key] = params
       end # class method define_action
       
+      # Lists the actions defined for the current controller by its base class.
+      # In most cases, the actions instance method should be used instead, as
+      # it handles class-based inheritance and can be overriden to implement
+      # controller-specific functionality, such as proxies or delegates.
       def actions
         @actions ||= {}
       end # class method actions
     end # module ClassMethods
     
+    # Lists the actions available to the current controller. Override this
+    # method to implement action redirection, e.g. through a proxy or delegate.
     def actions
       actions = {}
       
@@ -33,11 +49,26 @@ module Mithril::Controllers::Mixins
       actions
     end # method actions
     
+    # Wrapper method for actions.has_key?
     def has_action?(key)
       return false if key.nil?
       return self.actions.has_key? key.intern
     end # method has_action?
-
+    
+    # Searches for a matching action. If found, calls the action with the given
+    # session hash and arguments list.
+    # 
+    # === Parameters
+    # * session: Expects a hash (can be empty). Probably breaks if you pass in
+    #   nil, or something that isn't a hash.
+    # * command: Converted to a string. The converted string must be an exact
+    #   match (===) to the key passed in to klass.define_action.
+    # * args: Expects an array (can be empty). Probably breaks if you pass in
+    #   nil, or something that isn't an array.
+    # 
+    # === Returns
+    # The result of the action (should be a string), or nil if no action was
+    # invoked.
     def invoke_action(session, command, args)
       if self.has_action? command
         self.send :"action_#{command}", session, args
